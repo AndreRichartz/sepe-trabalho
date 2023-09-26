2cd3766e9c9a2f814e4d1918ded7803e4083278d
AndreRichartz <andrerichartz07@gmail.com>
2023-09-26 16:56:01 -0300

    dark mode and organization

--- js/script.js
+++ js/script.js
@@ -66,3 +66,18 @@
 
 })();
 
+const toggle = document.getElementById("toggle");
+const theme = window.localStorage.getItem("theme");
+
+
+if (theme === "dark") document.body.classList.add("dark");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  if (theme === "dark") {
+    window.localStorage.setItem("theme", "light");
+  } else window.localStorage.setItem("theme", "dark");
+});
+
+
+
